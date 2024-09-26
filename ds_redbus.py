2d0fc95b98f1a8b4cbf7e6e4233cb51511fcf4d8
srvtwiz_pc <srviknesh@gmail.com>
2024-09-26 18:36:57 +0530

    26.09.24

--- ds_redbus.py
+++ ds_redbus.py
@@ -4,6 +4,7 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 import pandas as pd
 import time
 
@@ -17,8 +18,6 @@ time.sleep(4)
 #APSRTC=driver.find_element(By.CLASS_NAME, 'rtcName')
 APSRTC_elements = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'rtcName')))
 data=[]
-length=len(APSRTC_elements)
-print(length)
 for element in APSRTC_elements:
     data.append(element.text)
     #element.click()
@@ -32,6 +31,9 @@ for i in data:
     print(i)
     elements = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, f'//div[@class="rtcName" and text()="{i}"]')))
     print(elements)
+    body = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="Carousel"]')))
+    body.click()
+    body.send_keys(Keys.ARROW_RIGHT)
     time.sleep(4)
     elements.click()
     driver.back()
@@ -43,4 +45,41 @@ for i in data:
 #route_1=driver.find_element(By.XPATH,"//a[@href='/bus-tickets/hyderabad-to-vijayawada']")
 
 #route_1.click()
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+# import time
 
+# # Initialize the Chrome driver
+# driver = webdriver.Chrome()
+
+# # Open the RedBus website
+# driver.get("https://www.redbus.in/online-booking/ksrtc-kerala/?utm_source=rtchometile")
+
+# wait = WebDriverWait(driver, 10)
+
+# # Wait until the nested div is present
+# nested_div = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.D117_main.D117_container')))
+
+# page = 1
+# all_route = []
+
+# while True:
+#     print(f"Processing page {page}")
+
+#     # Re-acquire the nested div after navigating to the next page
+#     nested_div = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.D117_main.D117_container')))
+
+#     # Find route links on the current page and collect them
+#     route_links = nested_div.find_elements(By.CSS_SELECTOR, 'div.route_link')
+
+#     all_route.extend(route_links)
+
+#     break
+
+# for route in all_route:
+#     link = route.find_element(By.TAG_NAME, 'a')
+#     route_name = link.get_attribute('title')
+#     route_link = link.get_attribute('href')
+#     print(route_name,route_link)
